7f97b53b3009796ccdd5807cd2d6fdaf35fade9a
781500109 <781500109@qq.com>
2020-05-22 11:34:36 +0800

    提交

---
+++ IterationPage/src/App.vue
@@ -0,0 +1,30 @@
+
+<template>
+<el-container style="height: 600px; border: 1px solid #eee">
+  <!--左边主菜单栏目-->
+  <el-aside width="150px" >
+    <sideBar class="card"/>
+  </el-aside>
+  <el-main>
+     <Task class="card"/>
+  </el-main>
+  </el-container>
+</template>
+
+<style>
+</style>
+
+<script>
+import Task from './components/Task'
+import sideBar from './components/sideBar'
+export default {
+  name: 'IterationPage',
+  components: {Task, sideBar},
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
---
+++ IterationPage/src/components/sideBar.vue
@@ -0,0 +1,35 @@
+<template>
+<el-container >
+  <!--左边主菜单栏目-->
+    <el-menu>
+    <el-menu-item index="1">
+      <i class="el-icon-location"></i>
+      <span slot="title">所有任务</span>
+    </el-menu-item>
+    <el-menu-item index="2">
+      <i class="el-icon-menu"></i>
+      <span slot="title">所有迭代</span>
+    </el-menu-item>
+    <el-menu-item index="3">
+      <i class="el-icon-document"></i>
+      <span slot="title">已完成任务</span>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <i class="el-icon-setting"></i>
+      <span slot="title">已完成迭代</span>
+    </el-menu-item>
+    </el-menu>
+</el-container>
+</template>
+
+<style>
+ .el-main {
+    background-color: #F5F5F5;
+ }
+</style>
+
+<script>
+export default {
+  name: 'MainMenu'
+}
+</script>
